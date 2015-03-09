59ef9efa0f14884daa434ffecdd2273fd69af567
cmkoller <cmkoller01@gmail.com>
2015-03-09 13:38:51 -0400

    First commit

---
+++ workout_summary.rb
@@ -0,0 +1,31 @@
+require_relative 'workout'
+require 'csv'
+require 'table_print'
+
+# create a hash of workout info from CSV
+def load_workout_data(filename)
+  workouts = []
+
+  CSV.foreach(filename, headers: true, header_converters: :symbol, converters: :numeric) do |row|
+    workout = workouts.find { |wo| wo.id == row[:workout_id] }
+
+    if workout.nil?
+      workout = Workout.new(row[:workout_id], row[:date])
+      workouts << workout
+    end
+
+    exercise = {
+      name: row[:exercise],
+      category: row[:category],
+      duration_in_min: row[:duration_in_min]
+    }
+
+    workout.exercises << exercise
+  end
+
+  workouts
+end
+
+workouts = load_workout_data("workouts.csv")
+
+tp(workouts, :id, :date, :type, :duration, :calories_burned)
